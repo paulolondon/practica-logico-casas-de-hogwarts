5b99a73e748c20e1b7c8914b4ba75e6b609632a6
MARIANACECILIAMATOS <matosm@ar.ibm.com>
2020-07-09 18:03:28 -0300

    Punto 1a con functores

--- src/laCopaDeLasCasas.pl
+++ src/laCopaDeLasCasas.pl
@@ -2,7 +2,32 @@
 %% Parte 2 - La copa de las casas
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+hizo(harry, fueraDeCama).
+hizo(hermione, irA(tercerPiso)).
+hizo(hermione, irA(seccionRestringida)).
+hizo(harry, irA(bosque)).
+hizo(harry, irA(tercerPiso)).
+hizo(draco, irA(mazmorras)).
+hizo(ron, buenaAccion(50, ganarAlAjedrezMagico)).
+hizo(hermione, buenaAccion(50, salvarASusAmigos)).
+hizo(harry, buenaAccion(60, ganarleAVoldemort)).
 
+hizoAlgunaAccion(Mago):-
+  hizo(Mago, _).
+hizoAlgoMalo(Mago):-
+  hizo(Mago, Accion),
+  puntajeQueGenera(Accion, Puntaje),
+  Puntaje < 0.
+
+puntajeQueGenera(fueraDeCama, -50).
+puntajeQueGenera(irA(Lugar), PuntajeQueResta):-
+  lugarProhibido(Lugar, Puntos),
+  PuntajeQueResta is Puntos * -1.
+puntajeQueGenera(buenaAccion(Puntaje, _), Puntaje).
+
+lugarProhibido(bosque, 50).
+lugarProhibido(seccionRestringida, 10).
+lugarProhibido(tercerPiso, 75).
 
 esBuenAlumno(Mago):-
   hizoAlgunaAccion(Mago),
