1ed86089c56d65e5c9204a5be42793f34b60e8ed
MARIANACECILIAMATOS <matosm@ar.ibm.com>
2020-07-10 18:11:43 -0300

    punto 1 parte 1

--- src/sombreroSeleccionador.pl
+++ src/sombreroSeleccionador.pl
@@ -1,3 +1,24 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Parte 1 - Sombrero Seleccionador
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+casa(gryffindor).
+casa(slytherin).
+casa(hufflepuff).
+casa(ravenclaw).
+
+sangre(harry, mestiza).
+sangre(draco, pura).
+sangre(hermione, impura).
+
+mago(Mago):-
+  sangre(Mago, _).
+
+% permiteEntrar(Casa, Mago)
+permiteEntrar(Casa, Mago):-
+  casa(Casa),
+  mago(Mago),
+  Casa \= slytherin.
+permiteEntrar(slytherin, Mago):-
+  sangre(Mago, TipoDeSangre),
+  TipoDeSangre \= impura.
