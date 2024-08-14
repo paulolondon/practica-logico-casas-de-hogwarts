d0f3afa56108b8cb332a0c788af43f3b47f976b2
paulolondon <tivenskis@frba.utn.edu.ar>
2024-08-14 11:39:25 -0300

    comit

--- src/laCopaDeLasCasas.pl
+++ src/laCopaDeLasCasas.pl
@@ -1,3 +1,59 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Parte 2 - La copa de las casas
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+accion(harry, fueraDeCama).
+accion(hermione, irA(tercerPiso)).
+accion(hermione, irA(seccionRestringida)).
+accion(harry, irA(bosque)).
+accion(harry, irA(tercerPiso)).
+accion(draco, irA(mazmorras)).
+accion(ron, buenaAccion(ganarAjedrezMagico, 50)).
+accion(hermione, buenaAccion(usarIntelecto, 50)).
+accion(harry, buenaAccion(ganarAVoldemort, 60)).
+
+% Lugares prohibidos
+lugarProhibido(bosque, 50).
+lugarProhibido(seccionRestringida, 10).
+lugarProhibido(tercerPiso, 75).
+
+% Casas de los alumnos
+esDe(hermione, gryffindor).
+esDe(ron, gryffindor).
+esDe(harry, gryffindor).
+esDe(draco, slytherin).
+esDe(luna, ravenclaw).
+
+% Cálculo de puntos por acción
+puntosAccion(fueraDeCama, -50).
+puntosAccion(irA(Lugar), Puntos) :-
+    lugarProhibido(Lugar, PuntosNeg),
+    Puntos is -PuntosNeg.
+puntosAccion(irA(_), 0).
+puntosAccion(buenaAccion(_, Puntos), Puntos).
+
+% Cálculo de puntos totales para un alumno
+puntosAlumno(Alumno, PuntosTotales) :-
+    accion(Alumno, _).
+    findall(Puntos, 
+            (accion(Alumno, Accion), puntosAccion(Accion, Puntos)), 
+            ListaPuntos),
+    sum_list(ListaPuntos, PuntosTotales).
+
+% Cálculo de puntos totales para una casa
+puntosCasa(Casa, PuntosTotales) :-
+    esDe(_, Casa).
+    findall(PuntosAlumno, 
+            (esDe(Alumno, Casa), puntosAlumno(Alumno, PuntosAlumno)), 
+            ListaPuntos),
+    sum_list(ListaPuntos, PuntosTotales).
+
+% Determinar la casa ganadora
+casaGanadora(Casa) :-
+    esDe(_, Casa).
+    findall(Puntos-C, puntosCasa(C, Puntos), Puntuaciones),
+    max_member(PuntosMax-Casa, Puntuaciones).
+
+buenAlumno(Mago) :-
+    accion(Mago, _),
+    not(accion(Mago,Accion),puntosAccion(Accion,Puntos), Puntos < 0)
--- src/sombreroSeleccionador.pl
+++ src/sombreroSeleccionador.pl
@@ -1,3 +1,67 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Parte 1 - Sombrero Seleccionador
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Parte 1 - Sombrero Seleccionador
+
+% Características de los magos
+caracteristica(harry, corajudo).
+caracteristica(harry, amistoso).
+caracteristica(harry, orgulloso).
+caracteristica(harry, inteligente).
+sangre(harry, mestiza).
+odia(harry, slytherin).
+
+caracteristica(draco, inteligente).
+caracteristica(draco, orgulloso).
+sangre(draco, pura).
+odia(draco, hufflepuff).
+
+caracteristica(hermione, inteligente).
+caracteristica(hermione, orgullosa).
+caracteristica(hermione, responsable).
+sangre(hermione, impura).
+
+% Características requeridas por las casas
+requiere(gryffindor, corajudo).
+requiere(slytherin, orgulloso).
+requiere(slytherin, inteligente).
+requiere(ravenclaw, inteligente).
+requiere(ravenclaw, responsable).
+requiere(hufflepuff, amistoso).
+
+mago(Mago):-
+    sangre(Mago, _).
+
+
+casa(Casa):-
+    requiere(Casa,_).
+
+permiteEntrada(Casa,Mago) :-
+    casa(Casa),
+    Casa \= slytherin.
+permiteEntrada(Casa,Mago) :-
+    sangre(Mago,Sangre),
+    Sangre \= impura.
+
+caracterApropiado(Mago, Casa) :-
+    casa(Casa),
+    mago(Mago),
+    forall(requiere(Casa, Caracteristica), 
+        caracteristica(Mago, Caracteristica)).
+
+puedeQuedarSeleccionado(Mago, Casa) :-
+    caracterApropiado(Casa,Mago),
+    permiteEntrada(Mago,Casa),
+    not(odia(Mago,Casa)).
+
+cadenaDeAmistades(Magos):-
+    forAll(member(Mago,Magos),caracteristica(Mago,amistoso)),
+    mismaCasa(Magos).
+
+cadenaMismaCasa([]).
+cadenaMismaCasa([_]).
+cadenaMismaCasa(Mago1, Mago2 | Magos):-
+    puedeQuedarSeleccionado(Mago1, Casa),
+    puedeQuedarSeleccionado(Mago2, Casa),
+    cadenaMismaCasa(Mago2 | Magos).
